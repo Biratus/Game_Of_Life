d30a60a3dfd3549d9dda5f72c7aa6fb96045b761
Biratus <clement.birette@hotmail.Fr>
2018-03-01 11:46:45 +0100

    clean

--- Structures.js
+++ Structures.js
@@ -1,28 +1,6 @@
-function block(pos) {
-    cells[pos.y][pos.x]=ALIVE;
-    cells[pos.y+1][pos.x]=ALIVE;
-    cells[pos.y][pos.x+1]=ALIVE;
-    cells[pos.y+1][pos.x+1]=ALIVE;
-}
-
-function blinker(pos) {
-    cells[pos.y][pos.x]=ALIVE;
-    cells[pos.y][pos.x-1]=ALIVE;
-    cells[pos.y][pos.x+1]=ALIVE;
-}
-
-function toad(pos) {
-    for(var i=pos.x;i<pos.x+3;i++) {
-        cells[pos.y+1][i]=ALIVE;
-    }
-    for(var i=pos.x+1;i<pos.x+4;i++) {
-        cells[pos.y][i]=ALIVE;
-    }
-}
-
 function beacon(pos) {
-    block(pos);
-    block({"x":pos.x+2,"y":pos.y+2});
+    make("block",pos);
+    make("block",{"x":pos.x+2,"y":pos.y+2});
 }
 
 var struct={};
@@ -44,6 +22,10 @@ function loadStruct() {
 function make(structName,pos) {
     if(!struct[structName]) {
         console.log("No structure with matching name: "+structName);
+        return;
+    } else if(strucName=="beacon") {
+        beacon(pos);
+        draw();
         return;
     }
     for(var i = 0; i<struct[structName].length;i++) {
